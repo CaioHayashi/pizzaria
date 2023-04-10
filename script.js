f59aa02d4cc9e1a4ac125d9acc68a406e0da96bf
CaioHayashi <84331740+CaioHayashi@users.noreply.github.com>
2023-04-09 23:21:45 -0300

    add flip card

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const card = document.getElementById("card");
-const back = document.getElementById("back");
-
+var isFront = true;
 card.addEventListener("click", () => {
-	card.style.transform = "rotateY(180deg)";
-});
+	const textCard = document.getElementById("text-card");
 
-back.addEventListener("click", () => {
-	back.style.transform = "rotateY(180deg)";
+	if (isFront == true) {
+		card.style.transform = "rotateY(180deg)";
+		card.style.backgroundImage = "none";
+		card.style.backgroundColor = "#eee";
+		textCard.style.opacity = "1";
+		isFront = false;
+	} else {
+		textCard.style.opacity = "0";
+		card.style.transform = "rotateY(360deg)";
+		card.style.backgroundImage =
+			"url(https://images.pexels.com/photos/13814644/pexels-photo-13814644.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)";
+		isFront = true;
+	}
 });
