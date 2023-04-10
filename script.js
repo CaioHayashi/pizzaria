55ebb14b310d0be474f75605c9e6a6140d6baaef
CaioHayashi <84331740+CaioHayashi@users.noreply.github.com>
2023-04-10 18:25:33 -0300

    FlipCard added

--- script.js
+++ script.js
@@ -1,19 +1,68 @@
-const card = document.getElementById("card");
-var isFront = true;
-card.addEventListener("click", () => {
-	const textCard = document.getElementById("text-card");
-
-	if (isFront == true) {
-		card.style.transform = "rotateY(180deg)";
-		card.style.backgroundImage = "none";
-		card.style.backgroundColor = "#eee";
-		textCard.style.opacity = "1";
-		isFront = false;
-	} else {
-		textCard.style.opacity = "0";
-		card.style.transform = "rotateY(360deg)";
-		card.style.backgroundImage =
-			"url(https://images.pexels.com/photos/13814644/pexels-photo-13814644.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)";
-		isFront = true;
+const cards = [
+	{
+		front:
+			"url(https://images.pexels.com/photos/4109111/pexels-photo-4109111.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)",
+		back: "pizza-1"
+	},
+	{
+		front:
+			"url(https://images.pexels.com/photos/2147491/pexels-photo-2147491.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)",
+		back: "pizza-2"
+	},
+	{
+		front:
+			"url(https://images.pexels.com/photos/13642133/pexels-photo-13642133.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)",
+		back: "pizza-3"
+	},
+	{
+		front:
+			"url(https://images.pexels.com/photos/2471171/pexels-photo-2471171.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)",
+		back: "pizza-4"
+	},
+	{
+		front:
+			"url(https://images.pexels.com/photos/6072095/pexels-photo-6072095.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)",
+		back: "pizza-5"
+	},
+	{
+		front:
+			"url(https://images.pexels.com/photos/9844834/pexels-photo-9844834.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)",
+		back: "pizza-6"
+	},
+	{
+		front:
+			"url(https://images.pexels.com/photos/4841736/pexels-photo-4841736.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)",
+		back: "pizza-7"
 	}
+];
+
+cards.map((card) => {
+	const container = document.getElementById("container-card");
+	const newCard = document.createElement("div");
+	newCard.classList.add("card");
+	container.appendChild(newCard);
+
+	const newText = document.createElement("p");
+	newText.classList.add("text-card");
+	newText.innerText = card.back;
+	newCard.appendChild(newText);
+
+	newCard.style.backgroundImage = card.front;
+	var isFront = true;
+
+	newCard.addEventListener("click", () => {
+		if (isFront == true) {
+			newCard.style.transform = "rotateY(180deg)";
+			newCard.style.backgroundImage = "none";
+			newText.style.opacity = "1";
+
+			isFront = false;
+		} else {
+			newCard.style.transform = "rotateY(360deg)";
+			newCard.style.backgroundImage = card.front;
+			newText.style.opacity = "0";
+
+			isFront = true;
+		}
+	});
 });
